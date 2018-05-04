cd9947a29626c2d31e90ea1726817adb97fba45b
bustardcelly <bustardcelly@gmail.com>
2018-05-04 09:25:37 -0400

    README update and file removal after broadcast.

--- script/rtmpbee-publisher.sh
+++ script/rtmpbee-publisher.sh
@@ -5,8 +5,8 @@
 #
 # USAGE: rtmpbee-publisher.sh [endpoint] [app] [streamName] [amount of streams to start] [amount of time to playback] [flv-file]
 #
-# EXAMPLE: ./rtmpbee-publisher.sh ipv6west.red5.org live stream1 10 10 bbb_480p.flv
-# LOCAL EXAMPLE: ./rtmpbee-publisher.sh localhost:1935 live stream1 10 10 bbb_480p.flv
+# EXAMPLE: ./rtmpbee-publisher.sh ipv6west.red5.org live stream1 10 30 bbb_480p.flv
+# LOCAL EXAMPLE: ./rtmpbee-publisher.sh localhost:1935 live stream1 30 10 bbb_480p.flv
 #
 # DESCRIPTION: Creates N-number of RTMP broadcast with file as a live stream.
 #
@@ -33,7 +33,8 @@ file=$6
 #===============================================================================
 function shutdown {
   pid=$1
-  kill -9 "$pid" && echo "PID(${pid}) stopped." || echo "Failure to kill ${pid}."
+  file=$2
+  kill -9 "$pid" && rm -f "$file" && echo "PID(${pid}) stopped." || echo "Failure to kill ${pid}."
   echo "Attack ended at $(date '+%d/%m/%Y %H:%M:%S')"
   return 0
 }
@@ -47,12 +48,13 @@ function shutdown {
 function set_timeout {
   id=$1
   t=$2
-  isLast=$3
+  f=$3
+  isLast=$4
   echo "Will kill ${id} in ${t} seconds..."
   if [ $isLast -eq 1 ]; then
-    (sleep "$t"; shutdown "$id" || echo "Failure to kill ${id}."; return 0)
+    (sleep "$t"; shutdown "$id" "$f" || echo "Failure to kill ${id}."; return 0)
   else
-    (sleep "$t"; kill -9 "$id" && echo "PID(${id}) stopped." || echo "Failure to kill ${id}.")&
+    (sleep "$t"; kill -9 "$id" && rm -f "$f" && echo "PID(${id}) stopped." || echo "Failure to kill ${id}.")&
   fi
   return 0
 }
@@ -67,14 +69,14 @@ for ((i=0;i<amount;i++)); do
   stream_file="${file}_${i}"
   cp "$file" "$stream_file"
   # </dev/null tells ffmpeg to not look for input
-  ffmpeg -re -stream_loop -1 -fflags +genpts -i "$stream_file" -c copy -f flv "$target" </dev/null > /dev/null 2>$1 &
+  ffmpeg -re -stream_loop -1 -fflags +genpts -i "$stream_file" -c copy -f flv "$target" 2>/dev/null &
   isLast=0
   if [ $i -eq $((amount - 1)) ]; then
     isLast=1
   fi
   pid=$!
   echo "Dispatching Bee $i($stream_file) at $target, PID(${pid})..."
-  set_timeout "$pid" "$timeout" $isLast
+  set_timeout "$pid" "$timeout" "$stream_file" $isLast
   sleep 0.2
 done
 
