5c3f58c1a95e33462daa74bcb3d30150f44a6bc7
bustardcelly <bustardcelly@gmail.com>
2018-05-04 09:11:54 -0400

    breakthrough.

--- script/rtmpbee-publisher.sh
+++ script/rtmpbee-publisher.sh
@@ -21,7 +21,7 @@
 set -e
 endpoint=$1
 app=$2
-streamName=$3
+stream_name=$3
 amount=$4
 timeout=$5
 file=$6
@@ -45,34 +45,36 @@ function shutdown {
 # PARAMETER 2: The amount of time to wait before killing process.
 #===============================================================================
 function set_timeout {
-  pid=$1
+  id=$1
   t=$2
   isLast=$3
-  echo "Will kill ${pid} in ${t} seconds..."
+  echo "Will kill ${id} in ${t} seconds..."
   if [ $isLast -eq 1 ]; then
-    (sleep "$t"; shutdown "$pid" || echo "Failure to kill ${pid}."; return 0)&
+    (sleep "$t"; shutdown "$id" || echo "Failure to kill ${id}."; return 0)
   else
-    (sleep "$t"; kill -9 "$pid" && echo "PID(${pid}) stopped." || echo "Failure to kill ${pid}.")&
+    (sleep "$t"; kill -9 "$id" && echo "PID(${id}) stopped." || echo "Failure to kill ${id}.")&
   fi
   return 0
 }
 
-# Dispatch.
-target="rtmp://${endpoint}/${app}/${streamName}"
-for ((i=0;i<amount;i++)); do
-  cp "$file" "${file}_${i}"
-  # </dev/null tells ffmpeg to not look for input
-  ffmpeg -re -stream_loop -1 -fflags +genpts -i "${file}_${i}" -c copy -f flv "$target" </dev/null > /dev/null 2>$1 &
-  isLast=0
-  if [ $i -eq $((amount - 1)) ]; then
-    echo "Is last one..."
-    isLast=1
-  fi
-  set_timeout $! "$timeout" $isLast
-  sleep 0.2
-  echo "Dispatching Bee $i at $target, PID(${pid})..."
-done
-
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 echo "Attack deployed at $dt"
+
+# Dispatch.
+for ((i=0;i<amount;i++)); do
+  name="${stream_name}_${i}"
+  target="rtmp://${endpoint}/${app}/${name}"
+  stream_file="${file}_${i}"
+  cp "$file" "$stream_file"
+  # </dev/null tells ffmpeg to not look for input
+  ffmpeg -re -stream_loop -1 -fflags +genpts -i "$stream_file" -c copy -f flv "$target" </dev/null > /dev/null 2>$1 &
+  isLast=0
+  if [ $i -eq $((amount - 1)) ]; then
+    isLast=1
+  fi
+  pid=$!
+  echo "Dispatching Bee $i($stream_file) at $target, PID(${pid})..."
+  set_timeout "$pid" "$timeout" $isLast
+  sleep 0.2
+done
 
